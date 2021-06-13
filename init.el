7d4b1ed9b9829cc998a67e8494c4c8526f87009a
Tim Hawes <trhawes@gmail.com>
2021-06-13 18:57:36 -0400

    Add prescient

--- init.el
+++ init.el
@@ -20,10 +20,12 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       company           ; the ultimate code completion backend
+       (company
+        +prescient)           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       (ivy +icons)      ; a search engine for love and life
+       (ivy +icons
+            +prescient)      ; a search engine for love and life
 
        :ui
        deft              ; notational velocity for Emacs
